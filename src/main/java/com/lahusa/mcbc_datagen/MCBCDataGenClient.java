1778728f64864ab2d3d09122d0fbf7f07f04f143
Lasse Huber-Saffer <lasse@huber-saffer.de>
2022-11-26 10:23:30 +0100

    Added clientside resolution command

--- src/main/java/com/lahusa/mcbc_datagen/MCBCDataGenClient.java
+++ src/main/java/com/lahusa/mcbc_datagen/MCBCDataGenClient.java
@@ -1,23 +1,38 @@
 package com.lahusa.mcbc_datagen;
 
+import com.lahusa.mcbc_datagen.command.DataGenCommand;
+import com.lahusa.mcbc_datagen.command.ResolutionCommand;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerInventory;
+
+import java.util.Objects;
 
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class MCBCDataGenClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Register network packet handlers
+        // Clientside commands
+        ClientCommandRegistrationCallback.EVENT.register(
+                (dispatcher, registryAccess) -> ResolutionCommand.register(dispatcher)
+        );
+
+        // Clientside network packet handlers
         // Selected slot update packet
         ClientPlayNetworking.registerGlobalReceiver(
-                MCBCDataGenMod.INVENTORY_SLOT_CHANGE_PACKET_ID,
-                (client, handler, buf, responseSender) -> {
-                    if (client.player != null) {
-                        PlayerInventory inventory = client.player.getInventory();
-                        inventory.selectedSlot = buf.readInt();
-                    }
+            MCBCDataGenMod.INVENTORY_SLOT_CHANGE_PACKET_ID,
+            (client, handler, buf, responseSender) -> {
+                if (client.player != null) {
+                    PlayerInventory inventory = client.player.getInventory();
+                    inventory.selectedSlot = buf.readInt();
                 }
+            }
         );
     }
 }
---
+++ src/main/java/com/lahusa/mcbc_datagen/command/ResolutionCommand.java
@@ -0,0 +1,19 @@
+package com.lahusa.mcbc_datagen.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.MinecraftClient;
+
+public class ResolutionCommand {
+    public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
+        dispatcher.register(
+            ClientCommandManager.literal("resolution").executes(
+                context -> {
+                    MinecraftClient.getInstance().getWindow().setWindowedSize(960, 540);
+                    return 1;
+                }
+            )
+        );
+    }
+}
