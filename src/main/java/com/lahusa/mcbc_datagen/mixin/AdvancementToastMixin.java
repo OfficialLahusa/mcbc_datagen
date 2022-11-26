ec772717c082bf19f156d10cde0ad82c2e3c7eec
Lasse Huber-Saffer <lasse@huber-saffer.de>
2022-11-26 08:12:06 +0100

    Fixed AdvancementToastMixin redirect error

--- src/main/java/com/lahusa/mcbc_datagen/mixin/AdvancementToastMixin.java
+++ src/main/java/com/lahusa/mcbc_datagen/mixin/AdvancementToastMixin.java
@@ -6,13 +6,15 @@ import net.minecraft.client.toast.ToastManager;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AdvancementToast.class)
 public class AdvancementToastMixin {
-
-    @Redirect(method = "draw", at = @At(value = "HEAD"))
-    public Toast.Visibility hideAllAdvancementToasts(MatrixStack matrices, ToastManager manager, long startTime) {
-        return Toast.Visibility.HIDE;
+    @Inject(method = "draw", at = @At(value = "HEAD"), cancellable = true)
+    public void hideAllAdvancementToasts(MatrixStack matrices, ToastManager manager, long startTime, CallbackInfoReturnable<Toast.Visibility> cir) {
+        cir.setReturnValue(Toast.Visibility.HIDE);
+        cir.cancel();
     }
 }
