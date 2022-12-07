78ffe95e0aa0051393b2efc5af68bb376a003cef
Lasse Huber-Saffer <lasse-huber-saffer@web.de>
2022-12-07 20:48:37 +0100

    Removed unnecessary dependency

--- src/main/java/com/lahusa/mcbc_datagen/mixin/InGameHudMixin.java
+++ src/main/java/com/lahusa/mcbc_datagen/mixin/InGameHudMixin.java
@@ -1,7 +1,6 @@
 package com.lahusa.mcbc_datagen.mixin;
 
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
