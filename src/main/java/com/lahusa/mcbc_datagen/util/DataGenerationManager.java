c99daa408977775d60141a279110f3e6e0f7cbb6
Sven Schreiber <sven-schreiber@outlook.de>
2022-12-12 12:41:54 +0100

    Reduced min total screenshots (5->3)

--- src/main/java/com/lahusa/mcbc_datagen/util/DataGenerationManager.java
+++ src/main/java/com/lahusa/mcbc_datagen/util/DataGenerationManager.java
@@ -35,7 +35,7 @@ public class DataGenerationManager {
     // 10 second delay
     private static final int WORLD_GEN_DELAY_TICKS = 200; // 10s
     private static final int RAND_DELAY_TICKS = 20; // 1s
-    private static final int MIN_TOTAL_SCREENSHOTS = 5;
+    private static final int MIN_TOTAL_SCREENSHOTS = 3;
     private static PlayerManager serverPlayerManager;
 
     static {
